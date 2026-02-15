30252e10551fd457082cba9c9fa3ef37d95c344d
Lazizbek Rahimov <lazizbekrakhimov25@gmail.com>
2026-02-15 09:26:29 +0500

    optimization v1.1

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -35,15 +35,15 @@ const TodoItem = ({ index, item }) => {
     }
 
     return (
-        <li className="task-card liquid-small flex justify-between items-center p-6 sm:p-7 rounded-3xl">
+        <li className="task-card liquid-small flex justify-between items-start p-6 sm:p-7 rounded-3xl">
 
-            <div className="flex items-center gap-5">
-                <label className="relative flex items-center justify-center w-6 h-6 sm:w-9 sm:h-9 cursor-pointer">
+            <div className="flex items-start gap-7 flex-1">
+                <label className="relative flex items-center justify-center w-6 h-6 sm:w-9 sm:h-9 cursor-pointer shrink-0">
                     <input
                         onChange={(evt) => handleStatusChange(evt, index)}
                         checked={item.isCompleted}
                         type="checkbox"
-                        className="peer appearance-none w-full h-full rounded-full border border-white/30  bg-white/5 backdrop-blur-xl transition duration-200 checked:bg-white checked:shadow-[0_0_20px_rgba(255,255,255,0.8)]"
+                        className="peer appearance-none w-7 h-7 rounded-full border border-white/30 bg-white/5 backdrop-blur-xl transition duration-200 checked:bg-white checked:shadow-[0_0_20px_rgba(255,255,255,0.8)]"
                     />
                     <Check
                         size={18}
@@ -51,17 +51,17 @@ const TodoItem = ({ index, item }) => {
                     />
                 </label>
 
-                <span className="text-white/90 w-10 font-medium tracking-wide text-[22px] sm:text-lg">
+                <span className="text-white/90 wrap-break-word break-all font-medium tracking-wide text-[22px] sm:text-lg">
                     {item.content}
                 </span>
             </div>
 
-            <div className="flex gap-5">
-                <button onClick={() => handleEdit(index)} className="text-white/40 hover:text-white cursor-pointer transition">
+            <div className="flex ml-5 gap-5 shrink-0 items-start">
+                <button onClick={() => handleEdit(index)} className="text-white/40 hover:text-white transition">
                     <Pencil size={24} />
                 </button>
 
-                <button onClick={() => handleDelete(index)} className="text-white/40 hover:text-red-400 cursor-pointer transition">
+                <button onClick={() => handleDelete(index)} className="text-white/40 hover:text-red-400 transition">
                     <Trash2 size={24} />
                 </button>
             </div>
